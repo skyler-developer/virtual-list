2b975c742b5bc4ad15b4ecd10f9e4479a73dff30
skyler <skylerhaut@foxmail.com>
2024-08-18 15:52:14 +0800

    feat: 完善功能

--- 虚拟列表/demo-2/src/fixed/index.jsx
+++ 虚拟列表/demo-2/src/fixed/index.jsx
@@ -1,25 +1,23 @@
-import FixedSizeList from './fixed-size-list';
+import React from "react";
+import FixedSizeList from "./fixed-size-list";
 
 const Row = ({ index, style, forwardRef }) => {
-  return (
-    <div className={index % 2 ? 'list-item-odd' : 'list-item-even'} style={style} ref={forwardRef}>
-      Row {index}
-    </div>
-  )
-}
+    return (
+        <div
+            className={index % 2 ? "list-item-odd" : "list-item-even"}
+            style={style}
+            ref={forwardRef}>
+            Row {index}
+        </div>
+    );
+};
 
 const App = () => {
-  return (
-    <FixedSizeList
-      className="list"
-      height={200}
-      width={200}
-      itemSize={50}
-      itemCount={1000}
-    >
-      {Row}
-    </FixedSizeList>
-  );
-}
+    return (
+        <FixedSizeList className="list" height={200} width={200} itemSize={50} itemCount={1000}>
+            {Row}
+        </FixedSizeList>
+    );
+};
 
 export default App;
--- 虚拟列表/demo-2/src/main.js
+++ 虚拟列表/demo-2/src/main.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import App from "./fixed/index.jsx";
+import App from "./fixed/index.jsx";
 // import App from "./dynamic/index.jsx";
-import App from "./variable/index.jsx";
+// import App from "./variable/index.jsx";
 import "./app.css";
 const root = ReactDOM.createRoot(document.getElementById("app"));
 root.render(<App />);
